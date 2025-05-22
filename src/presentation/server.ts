cb91646a80fd5c1613d36003306e89b2ec09bd7b
Jesus Puentes <jopt98@gmail.com>
2025-05-22 15:05:00 -0600

    Se agregó cors

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express';
 import path from 'path';
+import cors from 'cors';
 
 interface Options {
   port: number;
@@ -25,6 +26,7 @@ export class Server {
   async start() {
     //* Middlewares
     this.app.use( express.json() ); // raw
+    this.app.use( cors() )
     this.app.use( express.urlencoded({ extended: true }) ); // x-www-form-urlencoded
 
     //* Public Folder
